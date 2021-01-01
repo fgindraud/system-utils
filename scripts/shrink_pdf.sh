1f01d24552880428e6b5865831bc22d4c463d519
François Gindraud <francois.gindraud@gmail.com>
2021-01-01 10:57:24 +0100

    Updae xinitrc ; add script

---
+++ scripts/shrink_pdf.sh
@@ -0,0 +1,18 @@
+# in out dpi
+
+gs					\
+	  -q -dNOPAUSE -dBATCH -dSAFER		\
+	  -sDEVICE=pdfwrite			\
+	  -dCompatibilityLevel=1.3		\
+	  -dPDFSETTINGS=/screen			\
+	  -dEmbedAllFonts=true			\
+	  -dSubsetFonts=true			\
+	  -dAutoRotatePages=/None		\
+	  -dColorImageDownsampleType=/Bicubic	\
+	  -dColorImageResolution=$3		\
+	  -dGrayImageDownsampleType=/Bicubic	\
+	  -dGrayImageResolution=$3		\
+	  -dMonoImageDownsampleType=/Subsample	\
+	  -dMonoImageResolution=$3		\
+	  -sOutputFile="$2"			\
+	  "$1"
