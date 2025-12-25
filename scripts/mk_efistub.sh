9304713dd734a9130feef68775fa7547a502ccbb
François Gindraud <francois.gindraud@gmail.com>
2025-12-25 13:38:45 +0100

    pattern: HW upgrade
    Fix screen naming that changed with GPU.
    
    Network was already good with wildcard to enable any `en*` interface.

--- scripts/mk_efistub.sh
+++ scripts/mk_efistub.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/bash
 
-kernel_cmdline='root=/dev/mapper/cryptroot rw initrd=\amd-ucode.img initrd=\initramfs-linux.img'
+# 03/2024 : dropped initrd=amd-ucode, now provided by mkinitcpio in the main ramfs
+
+kernel_cmdline='root=/dev/mapper/cryptroot rw initrd=\initramfs-linux.img'
 
 # Create nvram boot entry
-efibootmgr --verbose --disk /dev/sda --part 1 --create --label 'Arch Linux' --loader /vmlinuz-linux --unicode "$kernel_cmdline"
+disk='/dev/disk/by-id/ata-Samsung_SSD_850_EVO_500GB_S2RBNX0JC37631P'
+efibootmgr --verbose --disk "$disk" --part 1 --create --label 'Arch Linux' --loader /vmlinuz-linux --unicode "$kernel_cmdline"
 
 # Create UEFI shell script
-echo "vmlinuz-linux $kernel_cmdline" > /boot/startup.nsh
+# echo "vmlinuz-linux $kernel_cmdline" > /boot/startup.nsh
 
